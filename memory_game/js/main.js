71a0d263d886a95b1c229270804ec7137f186f39
Miguel Torres moran <gerardomigueltorresmoran@Miguels-MacBook-Air.local>
2019-02-12 23:54:14 -0800

    java script added

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,19 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king"
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[3];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+if (cardsInPlay.length == 2){
+  if(cardOne === cardTwo){
+    alert("You found a match");
+  }
+  else{
+    alert("Sorry try again !");
+  }
+}
