cc66710bcc11883e069b81074fc838d9ed115b94
Miguel Torres moran <gerardomigueltorresmoran@Miguels-MacBook-Air.local>
2019-02-13 16:09:47 -0800

    code clener

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,18 +2,29 @@ console.log("Up and running!");
 
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
+
 var cardOne = cards[0];
 var cardTwo = cards[3];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
 
-if (cardsInPlay.length == 2){
-  if(cardOne === cardTwo){
-    alert("You found a match");
-  }
-  else{
-    alert("Sorry try again !");
-  }
+function flipCard(cardId){
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
 }
+
+function checkForMatch(){
+
+  if (cardsInPlay.length == 2){
+    if(cardsInPlay[0] === cardsInPlay[1]){
+      alert("You found a match");
+    }
+    else{
+      alert("Sorry try again !");
+    }
+  }
+
+}
+
+flipCard(0);
+flipCard(1);
+
+checkForMatch();
