a9f8d200a082a5d28aee22dc54218ec0c48f22c6
Miguel Torres moran <gerardomigueltorresmoran@Miguels-MacBook-Air.local>
2019-02-13 21:44:59 -0800

    images added

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,20 @@
 console.log("Up and running!");
 
-var cards = ["queen","queen","king","king"];
+var cards = [{rank:"queen",suit :"hearts", cardImg : "images/queen-of-hearts.png" },
+             {rank:"queen",suit :"diamonds", cardImg : "images/queen-of-diamonds.png" },
+             {rank:"king",suit :"hearts", cardImg : "images/king-of-hearts.png" },
+             {rank:"king",suit :"diamonds", cardImg : "images/king-of-diamonds.png" }];
+
 var cardsInPlay = [];
 
 var cardOne = cards[0];
 var cardTwo = cards[3];
 
 function flipCard(cardId){
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].suit);
+  console.log(cards[cardId].cardImg);
 }
 
 function checkForMatch(){
