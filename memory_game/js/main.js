78f4468534d84eac1b97a4d59c1777e3a070eaae
Miguel Torres moran <gerardomigueltorresmoran@Miguels-MacBook-Air.local>
2019-02-15 11:14:34 -0800

    functional

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -7,18 +7,29 @@ var cards = [{rank:"queen",suit :"hearts", cardImg : "images/queen-of-hearts.png
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[3];
 
-function flipCard(cardId){
+function createBoard(){
+  for(var i=0 ; i < cards.length; i++){
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src',"images/back.png");
+    cardElement.setAttribute('data-id',i);
+    cardElement.addEventListener('click',flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
+
+
+function flipCard(){
+  var cardId = this.getAttribute('data-id');
   console.log("User flipped " + cards[cardId].rank);
   cardsInPlay.push(cards[cardId].rank);
   console.log(cards[cardId].suit);
   console.log(cards[cardId].cardImg);
+  this.setAttribute('src',cards[cardId].cardImg);
+  checkForMatch();
 }
 
 function checkForMatch(){
-
   if (cardsInPlay.length == 2){
     if(cardsInPlay[0] === cardsInPlay[1]){
       alert("You found a match");
@@ -30,7 +41,4 @@ function checkForMatch(){
 
 }
 
-flipCard(0);
-flipCard(1);
-
-checkForMatch();
+createBoard();
